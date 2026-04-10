ff9ac49b18231130a661fef3e06fd2150428a15d
meelan7-7 <rajraijohn3132525@gmail.com>
2026-04-10 22:39:54 +0530

    changed he codes for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,35 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import BookCard from "../components/BookCard";
+
 function Home() {
-  return <h2>Welcome to Online Library</h2>;
+  const books = useSelector(state => state.books.books);
+
+  const categories = ["Fiction", "Non-Fiction", "Sci-Fi"];
+
+  return (
+    <div>
+      <h1>Welcome to Online Library</h1>
+
+      {/* Categories */}
+      <h2>Categories</h2>
+      <div>
+        {categories.map(cat => (
+          <Link key={cat} to={`/books/${cat}`} style={{ marginRight: "10px" }}>
+            {cat}
+          </Link>
+        ))}
+      </div>
+
+      {/* Popular Books */}
+      <h2>Popular Books</h2>
+      <div>
+        {books.slice(0, 3).map(book => (
+          <BookCard key={book.id} book={book} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
